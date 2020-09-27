10619694d57da99fdcb892d0f89000936902a3d4
Constantine <barr.develop@gmail.com>
2020-09-27 20:31:11 +0400

    played with unions

--- src/server.js
+++ src/server.js
@@ -11,16 +11,50 @@ const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    interface User {
+      id: String!
+      name: String!
+    }
+
+    type Admin implements User {
+      id: String!
+      name: String!
+      permissions: Boolean
+    }
+
+    type Client implements User {
+      id: String!
+      name: String!
+      subscription: String!
+    }
+
+    type Query {
+      getUsers: [User]!
+    }
+
     schema {
       query: Query
-      mutation: Mutation
     }
   `
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    typeDefs: [rootSchema],
+    resolvers: {
+      Query: {
+        getUsers: function() {
+          return [
+            { id: '1', name: 'John', permissions: true },
+            { id: '2', name: 'Edward', subscription: 'free' }
+          ]
+        }
+      },
+      User: {
+        __resolveType: function(user) {
+          return user.hasOwnProperty('subscription') ? 'Client' : 'Admin'
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
